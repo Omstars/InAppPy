e53be7ea37b6983589706c435cdfdd527913f39a
wdifruscio <difruscio.dev@gmail.com>
2021-03-22 16:07:47 -0700

    updated str formatting

--- inapppy/errors.py
+++ inapppy/errors.py
@@ -17,10 +17,10 @@ class InAppPyValidationError(InAppPyError):
         super().__init__(message, *args, **kwargs)
 
     def __str__(self):
-        return f"{self.__class__.__name__} {self.message} {self.raw_response}"
+        return "{}{}{}".format(self.__class__.__name__, self.message, self.raw_response)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(message={self.message!r}, raw_response={self.raw_response!r})"
+        return "{}{}{}".format(self.__class__.__name__, self.message, self.raw_response)
 
 
 class GoogleError(InAppPyValidationError):
